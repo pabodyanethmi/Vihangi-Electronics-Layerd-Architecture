da38fa1a9113f96504876f01c1ca487858959f7c
Pabodya Kandambi <youremail@example.com>
2026-02-19 13:15:25 +0530

    UserDAOImpl

---
+++ src/main/java/lk/ijse/vihangielectronics_ijse_76/dao/UserDAOImpl.java
@@ -0,0 +1,73 @@
+package lk.ijse.vihangielectronics_ijse_76.dao;
+
+import lk.ijse.vihangielectronics_ijse_76.dto.UserDto;
+import lk.ijse.vihangielectronics_ijse_76.util.CrudUtil;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAOImpl {
+    public boolean saveUser(UserDto userDTO) throws SQLException, ClassNotFoundException {
+        String sql = "INSERT INTO user ( userName, password, jobRoll, status,email) VALUES (?,?,?,?,?)";
+        return CrudUtil.execute(
+                sql,
+                //userDTO.getUser_Id(),
+                userDTO.getUserName(),
+                userDTO.getPassword(),
+                userDTO.getStatus(),
+                userDTO.getEmail(),
+                userDTO.getJobRoll()
+        );
+    }
+
+    public boolean updateUser(UserDto userDTO) throws SQLException, ClassNotFoundException {
+        String sql = "UPDATE user SET userName=?, password=?, jobRoll=?, status=? WHERE email=?";
+        return CrudUtil.execute(
+                sql,
+                userDTO.getUserName(),
+                userDTO.getPassword(),
+                userDTO.getJobRoll(),
+                userDTO.getStatus(),
+                userDTO.getEmail()
+        );
+    }
+
+    public boolean deleteUser(UserDto userId) throws SQLException, ClassNotFoundException {
+        String sql = "DELETE FROM User WHERE user_id=?";
+        return CrudUtil.execute(sql, userId);
+    }
+
+    public UserDto SearchUser(String userId) throws SQLException, ClassNotFoundException {
+        String sql = "SELECT * FROM User WHERE user_id=?";
+        ResultSet rs = CrudUtil.execute(sql, userId);
+
+        if (rs.next()) {
+            return new UserDto(
+                    rs.getString("user_id"),
+                    rs.getString("username"),
+                    rs.getString("password"),
+                    rs.getString("job_role"),
+                    rs.getString("status")
+            );
+        }
+        return null;
+    }
+
+    public List<UserDto> getAllUsers() throws SQLException, ClassNotFoundException {
+        ResultSet rs = CrudUtil.execute("SELECT * FROM User");
+        List<UserDto> userList = new ArrayList<>();
+
+        while (rs.next()) {
+            userList.add(new UserDto(
+                    rs.getString("user_id"),
+                    rs.getString("username"),
+                    rs.getString("password"),
+                    rs.getString("job_role"),
+                    rs.getString("status")
+            ));
+        }
+        return userList;
+    }
+}
